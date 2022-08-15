5eda891af841b137ed9d8a17b6421b852f1f428f
bnjmnm <benm@umich.edu>
2022-08-15 07:17:50 -0400

    add listeners

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
-console.log("we are in the JS file")
+const problem1 = () => {
+  console.log('problem 1');
+}
 
-const addNumbers = (first, second) => {
-  return first + second;
+const problem2 = () => {
+  console.log('problem 2');
+}
+
+const problem3 = () => {
+  console.log('problem 3');
+}
+
+const problem4 = () => {
+  console.log('problem 4');
+}
+
+
+
+
+
+
+
+// Don't change below.
+for (let i = 1; i < 5; i++) {
+  document.querySelector(`#button-${i}`)
+    .addEventListener('click', () => {
+      eval(`problem${i}()`)
+    });
 }
 
 if(typeof module !== 'undefined') {
